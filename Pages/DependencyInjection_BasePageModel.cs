160ea0a523274c3172b9bc942228ccc402d9492a
Jakob <jakobfred1999@gmail.com>
2023-10-11 15:20:19 +1300

    DI base page model to transfer data to pages

---
+++ Pages/DependencyInjection_BasePageModel.cs
@@ -0,0 +1,25 @@
+﻿using ArgoCMS.Data;
+using ArgoCMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CompanyManagementSystem.Pages
+{
+    public class DependencyInjection_BasePageModel : PageModel
+    {
+        protected ApplicationDbContext Context { get; }
+        protected IAuthorizationService AuthorizationService { get; }
+        protected UserManager<Employee> UserManager { get; }
+
+        public DependencyInjection_BasePageModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<Employee> userManager) : base()
+        {
+            Context = context;
+            AuthorizationService = authorizationService;
+            UserManager = userManager;
+        }
+    }
+}
