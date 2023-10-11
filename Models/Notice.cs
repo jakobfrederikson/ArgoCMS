bccc417b68e2e08d85d85cfd3d90e41956086e2d
Jakob <jakobfred1999@gmail.com>
2023-10-11 13:11:03 +1300

    Add models

--- Models/Notice.cs
+++ Models/Notice.cs
@@ -1,6 +1,27 @@
-﻿namespace ArgoCMS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArgoCMS.Models
 {
     public class Notice
     {
+        public int NoticeId { get; set; }
+
+        [Required]
+        [Display(Name = "Created by")]
+        public string OwnerID { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public string NoticeTitle { get; set; }
+
+        [Required]
+        [Display(Name = "Message content")]
+        [DataType(DataType.MultilineText)]
+        public string NoticeMessageContent { get; set; }
+
+        [Required]
+        [Display(Name = "Date created")]
+        [DataType(DataType.Date)] 
+        public DateTime DateCreated { get; set; }
     }
 }
