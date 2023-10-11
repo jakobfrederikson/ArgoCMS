b57ceccb9e6154ef56df52db61d30b300a26f922
Jakob <jakobfred1999@gmail.com>
2023-10-11 13:13:06 +1300

    Add services, authorization handling, and database seeding

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using ArgoCMS.Authorization.Jobs;
+using ArgoCMS.Authorization.Employees;
 using ArgoCMS.Data;
+using ArgoCMS.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,11 +14,54 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<Employee>(
+    options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddRazorPages();
 
+// Require authorization unless specified.
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+
+// Authorization handlers.
+// Assignments
+builder.Services.AddScoped<IAuthorizationHandler,
+                        JobIsOwnerAuthorizationHandler>();
+
+builder.Services.AddSingleton<IAuthorizationHandler,
+                        JobAdministratorAuthorizationHandler>();
+
+builder.Services.AddSingleton<IAuthorizationHandler,
+                        JobManagerAuthorizationHandler>();
+// Employees
+builder.Services.AddScoped<IAuthorizationHandler,
+                    EmployeeIsOwnerAuthorizationHandler>();
+
+builder.Services.AddSingleton<IAuthorizationHandler,
+                    EmployeeAdministratorAuthorizationHandler>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+    context.Database.Migrate();
+    // requires using Microsoft.Extensions.Configuration;
+    // Set password with the Secret Manager tool.
+    // dotnet user-secrets set SeedUserPW <pw>
+
+    var testUserPw = builder.Configuration.GetValue<string>("SeedUserPW");
+
+    await SeedData.Initialize(services, testUserPw);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
