db0f64a1cbcd0f50561e7f2dea4226651b0c1a97
Jakob <jakobfred1999@gmail.com>
2023-10-11 15:19:18 +1300

    Update models with fields

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,6 +1,45 @@
-﻿namespace ArgoCMS.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArgoCMS.Models
 {
-    public class Employee
+    public class Employee : IdentityUser
     {
+        // user ID of the person they report to
+        [Required]
+        [Display(Name = "Reports to")]
+        public string ReportsToId { get; set; }
+
+        [Required]
+        [Display(Name = "Team ID")]
+        public int TeamID { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Personal email")]
+        public string PersonalEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Employment date")]
+        public DateTime EmploymentDate { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public ICollection<Job> Jobs { get; set; }
+        public ICollection<Project> Projects { get; set; }
     }
 }
--- Models/Team.cs
+++ Models/Team.cs
@@ -1,6 +1,28 @@
-﻿namespace ArgoCMS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArgoCMS.Models
 {
     public class Team
     {
+        public int TeamId { get; set; }
+
+        [Display(Name = "Created by")]
+        public string OwnerID { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string TeamName { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string TeamDescription { get; set; }
+
+        [Required]
+        [Display(Name = "Date created")]
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+        public ICollection<Project> Projects { get; set; }
     }
 }
