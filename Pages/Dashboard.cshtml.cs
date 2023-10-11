d7ede0911a166d67728ec1740e2a5ca570a5d2cf
Jakob <jakobfred1999@gmail.com>
2023-10-11 15:19:37 +1300

    Add initial dashboard data

---
+++ Pages/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CompanyManagementSystem.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
