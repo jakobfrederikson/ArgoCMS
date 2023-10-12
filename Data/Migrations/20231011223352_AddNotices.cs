b166fe181ca68822b75c439619c93a57aedf454b
Jakob <jakobfred1999@gmail.com>
2023-10-12 19:26:18 +1300

    Add notices model

---
+++ Data/Migrations/20231011223352_AddNotices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArgoCMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notices",
+                columns: table => new
+                {
+                    NoticeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NoticeTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NoticeMessageContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notices", x => x.NoticeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notices");
+        }
+    }
+}
