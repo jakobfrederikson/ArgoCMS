cad3852aaba6298cd182f26635327e96f1e68121
Jakob <jakobfred1999@gmail.com>
2023-10-17 08:41:21 +1300

    Update Identity scaffold pages to use custom identity model

---
+++ Areas/Identity/Pages/Account/Manage/DbContextPageModel.cs
@@ -0,0 +1,19 @@
+﻿using ArgoCMS.Data;
+using ArgoCMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArgoCMS.Areas.Identity.Pages.Account.Manage
+{
+    public class DbContextPageModel : PageModel
+    {
+        protected ApplicationDbContext Context { get; }
+
+        public DbContextPageModel(
+            ApplicationDbContext context) : base()
+        {
+            Context = context;
+        }
+    }
+}
