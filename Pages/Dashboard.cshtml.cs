5fc4b12439d2dd469d9bca322f947d31d977bc35
Jakob <jakobfred1999@gmail.com>
2023-10-17 14:31:18 +1300

    Add bar chart for completed jobs in users team. Add company stats at top of dashboard page.

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -18,11 +18,18 @@ namespace CompanyManagementSystem.Pages
         {
         }
 
-        public IDictionary<string, JobStatus> Jobs { get; set; }
+        public IDictionary<string, JobStatus> UserJobStatus { get; set; }
+        public IDictionary<string, int> TeamMemberJobCompletion { get; set; }
         public List<Project> Projects { get; set; }
         public Team Team { get; set; }
         public Employee ReportsTo { get; set; }
         public IList<Notice> Notices { get; set; }
+        public List<string> BackgroundColours { get; set; }
+
+        public int TotalEmployees { get; set; }
+        public int TotalTeams { get; set; }
+        public int TotalJobs { get; set; }
+        public int TotalProjects { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -33,7 +40,9 @@ namespace CompanyManagementSystem.Pages
                 return NotFound($"Could not find user with id of: {userId}");
             }
 
-            var jobDict = InitializeDictionary();
+            var teamJobDict = InitializeTeamJobsDictionary(user);
+            var colours = InitializeBackgroundColours(teamJobDict.Keys.Count());
+            var jobDict = InitializeUserJobsDictionary();
 
             foreach (var job in Context.Jobs)
             {
@@ -53,9 +62,19 @@ namespace CompanyManagementSystem.Pages
 
             var notices = Context.Notices.ToList();
 
+            if (teamJobDict != null)
+            {
+                TeamMemberJobCompletion = teamJobDict;
+            }
+
+            if (colours != null)
+            {
+                BackgroundColours = colours;
+            }
+
             if (jobDict != null)
             {
-                Jobs = jobDict;
+                UserJobStatus = jobDict;
             }
 
             if (projects != null)
@@ -78,10 +97,17 @@ namespace CompanyManagementSystem.Pages
                 Notices = notices;
             }
 
+            TotalEmployees = Context.Employees.Count();
+            TotalTeams = Context.Teams.Count();
+            TotalJobs = Context.Jobs
+                .Where(j => j.JobStatus == JobStatus.Completed)
+                .Count();
+            TotalProjects = Context.Projects.Count();
+
             return Page();
         }
 
-        private Dictionary<string, JobStatus> InitializeDictionary()
+        private Dictionary<string, JobStatus> InitializeUserJobsDictionary()
         {
             var jobDict = new Dictionary<string, JobStatus>();
 
@@ -93,6 +119,52 @@ namespace CompanyManagementSystem.Pages
 
 
             return jobDict;
+        }
+
+        private Dictionary<string, int> InitializeTeamJobsDictionary(Employee currentUser)
+        {
+            Employee[] teamMembers = Context.Employees
+                                    .Where(e => e.TeamID == currentUser.TeamID)
+                                    .Include(e => e.Jobs.Where(j => j.JobStatus == JobStatus.Completed))
+                                    .ToArray();
+
+            var teamMemberJobsCompleted = new Dictionary<string, int>();
+
+            foreach (Employee teamMember in teamMembers)
+            {
+                teamMemberJobsCompleted[teamMember.FullName] = teamMember.Jobs.Count();
+            }            
+
+            return teamMemberJobsCompleted;
+        }
+
+        private List<string> InitializeBackgroundColours(int count)
+        {
+            List<string> listOfColours = new List<string>();
+            string opacity = "0.9";
+
+            string[] colours = new string[]
+            {
+                $"rgba(199, 255, 69, {opacity})",
+                $"rgba(134, 255, 69, {opacity})",
+                $"rgba(69, 255, 122, {opacity})",
+                $"rgba(69, 255, 227, {opacity})",
+                $"rgba(69, 171, 255, {opacity})",
+                $"rgba(69, 75, 255, {opacity})",
+                $"rgba(150, 69, 255, {opacity})",
+                $"rgba(230, 69, 255, {opacity})"
+            };
+
+            int counter = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (counter >= colours.Length) counter = 0;
+
+                listOfColours.Add(colours[counter]);
+                counter++;
+            }
+
+            return listOfColours;
         }
     }    
 }
