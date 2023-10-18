4802c0684635dfcc0a42676d2529f472eaa1ef53
Jakob <jakobfred1999@gmail.com>
2023-10-18 15:37:26 +1300

    Fix namespace

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
-namespace CompanyManagementSystem.Pages
+namespace ArgoCMS.Pages
 {
     public class DashboardModel : DependencyInjection_BasePageModel
     {
