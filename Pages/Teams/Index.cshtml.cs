875b5aeaf408879f64b7350b7cd6b5e8b44fcc25
Jakob <jakobfred1999@gmail.com>
2023-10-24 16:07:03 +1300

    Display all notices related to a team

--- Pages/Teams/Index.cshtml.cs
+++ Pages/Teams/Index.cshtml.cs
@@ -1,31 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ArgoCMS.Data;
 using ArgoCMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace ArgoCMS.Pages.Teams
 {
-    public class IndexModel : PageModel
+    public class IndexModel : DependencyInjection_BasePageModel
     {
-        private readonly ArgoCMS.Data.ApplicationDbContext _context;
-
-        public IndexModel(ArgoCMS.Data.ApplicationDbContext context)
+        public IndexModel(
+        ApplicationDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<Employee> userManager)
+        : base(context, authorizationService, userManager)
         {
-            _context = context;
         }
 
-        public IList<Team> Team { get;set; } = default!;
+        public Team Team { get; set; } = default!;
+        public IList<Job> TeamJobs { get; set;} = default!;
+        public IList<Notice> TeamNotices { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            if (_context.Teams != null)
+            var userId = UserManager.GetUserId(User);
+
+            var user = await Context.Employees.FirstOrDefaultAsync(e => e.Id == userId);
+
+            var team = await Context.Teams
+                .Include(t => t.Employees.Where(e => e.TeamID == user.TeamID))
+                .FirstOrDefaultAsync(t => t.TeamId == user.TeamID);
+
+            if (team != null)
             {
-                Team = await _context.Teams.ToListAsync();
+                Team = team;
+            }
+
+            var teamJobs = Context.Jobs
+                .Where(j => j.TeamID == Team.TeamId).ToList();
+
+            if (teamJobs != null)
+            {
+                TeamJobs = teamJobs;
+            }
+
+            var teamNotices = Context.Notices
+                .Where(n => n.TeamId == Team.TeamId).ToList();
+
+            if (teamNotices != null)
+            {
+                TeamNotices = teamNotices;
             }
         }
     }
