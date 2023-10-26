abe6d4bf76d3899119882479e208d8b9053f9a42
Jakob <jakobfred1999@gmail.com>
2023-10-26 15:41:15 +1300

    Add colour coding and links to job assignees and creators accounts.

--- Pages/Jobs/Index.cshtml.cs
+++ Pages/Jobs/Index.cshtml.cs
@@ -1,26 +1,85 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using ArgoCMS.Models;
+using ArgoCMS.Data;
 using Microsoft.EntityFrameworkCore;
-using ArgoCMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace ArgoCMS.Pages.Jobs
 {
-    public class IndexModel : PageModel
+    public class IndexModel : DependencyInjection_BasePageModel
     {
-        private readonly ArgoCMS.Data.ApplicationDbContext _context;
-
-        public IndexModel(ArgoCMS.Data.ApplicationDbContext context)
+        public IndexModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<Employee> userManager)
+            : base(context, authorizationService, userManager)
         {
-            _context = context;
         }
 
-        public IList<Job> Job { get;set; } = default!;
+        public IDictionary<Job, string> Jobs { get;set; } = default!;
+        public IDictionary<string, string> CreatedByIdToFullName { get; set; } = default!;
+        public IDictionary<string, string> AssignedToIdToFullName { get; set; } = default!;
+        public IDictionary<int, string> TeamsToFullName { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            if (_context.Jobs != null)
+            var userId = UserManager.GetUserId(User);
+            if (Context.Jobs != null)
             {
-                Job = await _context.Jobs.ToListAsync();
+                Jobs = await Context.Jobs
+                    .Where(j => j.JobStatus != JobStatus.Completed)
+                    .Where(j => j.OwnerID == userId || j.EmployeeID == userId)
+                    .OrderByDescending(j => (int) (j.PriorityLevel))
+                    .ToDictionaryAsync
+                    (
+                        j => j,
+                        j =>
+                        {
+                            switch (j.JobStatus)
+                            {
+                                case JobStatus.Unread:
+                                    return "table-primary";
+                                case JobStatus.Read:
+                                    return "table-danger";
+                                case JobStatus.Working:
+                                    return "table-warning";
+                                case JobStatus.Submitted:
+                                    return "table-success";
+                                default:
+                                    return "";
+                            }
+
+                        }
+                    );
             }
+
+            var createdByIdToFullName = new Dictionary<string, string>();
+            var assignedToIdToFullName = new Dictionary<string, string>();
+            var teamsToFullName = new Dictionary<int, string>();
+            foreach (var job in Jobs.Keys)
+            {
+                if (!createdByIdToFullName.ContainsKey(job.OwnerID))
+                {
+                    createdByIdToFullName[job.OwnerID] = Context.Employees
+                                .FirstOrDefault(j => j.Id == job.OwnerID).FullName;
+                }
+
+                if (!assignedToIdToFullName.ContainsKey(job.EmployeeID))
+                {
+                    assignedToIdToFullName[job.EmployeeID] = Context.Employees
+                                .FirstOrDefault(j => j.Id == job.EmployeeID).FullName;
+                }
+
+                if (!teamsToFullName.ContainsKey(job.TeamID))
+                {
+                    teamsToFullName[job.TeamID] = Context.Teams
+                                .FirstOrDefault(t => t.TeamId == job.TeamID).TeamName;
+                }
+            }
+
+            CreatedByIdToFullName = createdByIdToFullName;       
+            AssignedToIdToFullName = assignedToIdToFullName;
+            TeamsToFullName = teamsToFullName;
         }
     }
 }
