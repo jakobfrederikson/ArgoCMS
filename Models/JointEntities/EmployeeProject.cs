47dfceaa72167bfe93ac980ac66a29ed55c6661c
Jakob <jakobfred1999@gmail.com>
2023-11-01 15:59:07 +1300

    Add joint entities for data relationships

---
+++ Models/JointEntities/EmployeeProject.cs
@@ -0,0 +1,13 @@
+﻿namespace ArgoCMS.Models.JointEntities
+{
+	public class EmployeeProject
+	{
+		public string EmployeeId { get; set; }
+		public Employee Employee { get; set; }
+
+		public int ProjectId { get; set; }
+		public Project Project { get; set; }
+
+		public bool IsCompleted { get; set; }
+	}
+}
---
+++ Models/JointEntities/TeamProject.cs
@@ -0,0 +1,11 @@
+﻿namespace ArgoCMS.Models.JointEntities
+{
+	public class TeamProject
+	{
+		public int TeamId { get; set; }
+		public Team Team { get; set; }
+
+		public int ProjectId { get; set; }
+		public Project Project { get; set; }
+	}
+}
