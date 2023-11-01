3147f19a512e3be2fe34b5ce0857a8c5a6808635
Jakob <jakobfred1999@gmail.com>
2023-11-01 13:09:07 +1300

    Break logic into async methods

--- Pages/Teams/Index.cshtml.cs
+++ Pages/Teams/Index.cshtml.cs
@@ -23,70 +23,97 @@ namespace ArgoCMS.Pages.Teams
 
         public async Task OnGetAsync(int? id)
         {
-            var userId = UserManager.GetUserId(User);
-
-            var user = await Context.Employees.FirstOrDefaultAsync(e => e.Id == userId);
-
-            var team = new Team();
-
-            if (id != null)
-            {
-                team = await Context.Teams
-                .Include(t => t.Employees.Where(e => e.TeamID == id))
-                .FirstOrDefaultAsync(t => t.TeamId == id);
-            }
-            else
-            {
-                team = await Context.Teams
-                .Include(t => t.Employees.Where(e => e.TeamID == user.TeamID))
-                .FirstOrDefaultAsync(t => t.TeamId == user.TeamID);
-            }
-            
-
+            var team = await AcquireTeam(id);
             if (team != null)
             {
                 Team = team;
             }
 
-            var teamJobs = await Context.Jobs
-                .Where(j => j.TeamID == Team.TeamId)
-                .ToDictionaryAsync(
-                j => j,
-                j => Context.Employees
-                        .Where(e => e.Id == j.EmployeeID)
-                        .Single()
-                        .FullName
-                );
-
+            var teamJobs = await AcquireTeamsJobs();
             if (teamJobs != null)
             {
                 TeamJobs = teamJobs;
             }
 
-            var teamNotices = await Context.Notices
-                .Where(n => n.TeamId == Team.TeamId)
-                .ToDictionaryAsync(
-                x => x, 
-                x => Context.Employees.Where(
-                    e => e.Id == x.OwnerID)
-                .Single().FullName);
-
+            var teamNotices = await AcquireTeamNotices();
             if (teamNotices != null)
             {
                 TeamNotices = teamNotices;
             }
 
-            var employeeRole = await Context.Employees
-                .Where(e => e.TeamID == Team.TeamId)
-                .ToDictionaryAsync(
-                e => e,
-                r => string.Join(",", UserManager.GetRolesAsync(r).Result.ToArray())
-                );
-
+            var employeeRole = await AcquireEmployeesAndRoles();
             if (employeeRole != null)
             {
                 EmployeeAndRole = employeeRole;
             }
         }
+
+        private async Task<Team> AcquireTeam(int? id)
+        {
+				if (id != null)
+				{
+					var team = await Context.Teams
+					.Include(t => t.Employees.Where(e => e.TeamID == id))
+					.FirstOrDefaultAsync(t => t.TeamId == id);
+
+                    return team;
+				}
+				else
+				{
+					var userId = UserManager.GetUserId(User);
+					var user = await Context.Employees.FirstOrDefaultAsync(e => e.Id == userId);
+					var team = await Context.Teams
+					.Include(t => t.Employees.Where(e => e.TeamID == user.TeamID))
+					.FirstOrDefaultAsync(t => t.TeamId == user.TeamID);
+
+                    return team;
+				}
+        }
+
+        private async Task<Dictionary<Job, string>> AcquireTeamsJobs()
+        {
+
+                var result = await Context.Jobs
+				.Where(j => j.TeamID == Team.TeamId)
+				.ToDictionaryAsync(
+				j => j,
+				j => Context.Employees
+						.Where(e => e.Id == j.EmployeeID)
+						.Single()
+						.FullName
+				);
+
+                return result;
+
+        }
+
+        private async Task<Dictionary<Notice, string>> AcquireTeamNotices()
+        {
+
+			var result = await Context.Notices
+				.Where(n => n.TeamId == Team.TeamId)
+				.ToDictionaryAsync(
+				x => x,
+				x => Context.Employees.Where(
+					e => e.Id == x.OwnerID)
+				.Single().FullName);
+
+            return result;
+
+        }
+
+        private async Task<Dictionary<Employee, string>> AcquireEmployeesAndRoles()
+        {
+
+            var result = await Context.Employees
+				.Where(e => e.TeamID == Team.TeamId)
+				.ToDictionaryAsync(
+				e => e,
+				r => string.Join(",", UserManager.GetRolesAsync(r).Result.ToArray())
+				);
+
+            return result;
+
+		}
     }
 }
