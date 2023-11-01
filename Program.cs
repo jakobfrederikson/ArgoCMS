0bd7fc421d231243fe0a8c943396fcdb1ed020d2
Jakob <jakobfred1999@gmail.com>
2023-11-01 16:37:17 +1300

    Commenting out ensurecreated and migrate

--- Program.cs
+++ Program.cs
@@ -75,8 +75,8 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ApplicationDbContext>();
-    context.Database.EnsureCreated();
-    context.Database.Migrate();
+    //context.Database.EnsureCreated();
+    //context.Database.Migrate();
     // requires using Microsoft.Extensions.Configuration;
     // Set password with the Secret Manager tool.
     // dotnet user-secrets set SeedUserPW <pw>
