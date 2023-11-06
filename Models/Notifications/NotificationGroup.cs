cc35117b46e711bc5187649ef99d5028ca8679ab
Jakob <jakobfred1999@gmail.com>
2023-11-06 17:10:39 +1300

    Add notice groups for SignalR group signalling

---
+++ Models/Notifications/NotificationGroup.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArgoCMS.Models.Notifications
+{
+    public class NotificationGroup
+    {
+        public int Id { get; set; }
+        public string GroupName { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
