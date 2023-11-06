49f627bd1c1e6ffb19e58187ad719aa642398f61
Jakob <jakobfred1999@gmail.com>
2023-11-06 17:13:45 +1300

    Add new base page model for notifications

---
+++ Pages/DependencyInjection_Hub_BasePageModel .cs
@@ -0,0 +1,30 @@
+﻿using ArgoCMS.Data;
+using ArgoCMS.Hubs;
+using ArgoCMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ArgoCMS.Pages
+{
+    public class DependencyInjection_Hub_BasePageModel : PageModel
+    {
+        protected ApplicationDbContext Context { get; }
+        protected IAuthorizationService AuthorizationService { get; }
+        protected UserManager<Employee> UserManager { get; }
+        protected IHubContext<NotificationHub> HubContext { get; }
+
+        public DependencyInjection_Hub_BasePageModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<Employee> userManager,
+            IHubContext<NotificationHub> hubContext) : base()
+        {
+            Context = context;
+            AuthorizationService = authorizationService;
+            UserManager = userManager;
+            HubContext = hubContext;
+        }
+    }
+}
