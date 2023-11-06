9c409909f480bb16fae7427823b71ac8af12b28a
Jakob <jakobfred1999@gmail.com>
2023-11-06 17:09:33 +1300

    Add EmployeeTeam for multi-team users

---
+++ Models/JointEntities/EmployeeTeam.cs
@@ -0,0 +1,11 @@
+﻿namespace ArgoCMS.Models.JointEntities
+{
+    public class EmployeeTeam
+    {
+        public string EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+    }
+}
