de1bb7879d4e6f2e03b17854406e970413813cde
Jakob <jakobfred1999@gmail.com>
2023-11-07 14:02:36 +1300

    Show/Hide dropdowns based on PublicityStatus

--- Pages/Notices/Create.cshtml.cs
+++ Pages/Notices/Create.cshtml.cs
@@ -1,39 +1,101 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ArgoCMS.Models;
+using ArgoCMS.Models.Notifications;
+using ArgoCMS.Data;
+using ArgoCMS.Hubs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArgoCMS.Pages.Notices
 {
-    public class CreateModel : PageModel
+    public class CreateModel : DependencyInjection_Hub_BasePageModel
     {
-        private readonly ArgoCMS.Data.ApplicationDbContext _context;
-
-        public CreateModel(ArgoCMS.Data.ApplicationDbContext context)
+        public CreateModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<Employee> userManager,
+            IHubContext<NotificationHub> hubContext)
+            : base(context, authorizationService, userManager, hubContext)
         {
-            _context = context;
         }
 
         public IActionResult OnGet()
         {
+            TeamItems = new SelectList(Context.Teams, "TeamId", "TeamName");
+            ProjectItems = new SelectList(Context.Projects, "ProjectId", "ProjectName");
+
             return Page();
         }
 
         [BindProperty]
         public Notice Notice { get; set; }
-        
+        public SelectList TeamItems { get; set; }
+        public SelectList ProjectItems { get; set; }
+
+        public NoticeNotification NoticeNotification { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+            var user = await UserManager.GetUserAsync(User);
+
+            if (user == null)
             {
-                return Page();
+                return NotFound($"Current user doesn't exist");
             }
 
-            _context.Notices.Add(Notice);
-            await _context.SaveChangesAsync();
+            Notice.OwnerID = user.Id;
+            Notice.DateCreated = DateTime.Now; 
+
+            Context.Notices.Add(Notice);
+            await Context.SaveChangesAsync();
+
+            var notification = new NoticeNotification();
+            notification.Message = $"You have a new notice: {Notice.NoticeTitle}";
+            notification.URL = "/Notices/NoticeDetails";
+            notification.ObjectId = Notice.NoticeId.ToString();
+            notification.CompanyWide = Notice.PublicityStatus == PublicityStatus.Company;            
+
+            Context.NoticeNotifications.Add(notification);
+
+            string groupName = DetermineGroupNameForNotification(notification);
+            await HubContext.Clients.Group(groupName).SendAsync("ReceiveNoticeNotification", 
+                JsonConvert.SerializeObject(notification));
+            
+            await Context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
+        }
+
+        private string DetermineGroupNameForNotification(
+            NoticeNotification noticeNotification)
+        {
+            if (noticeNotification.CompanyWide) return "Company";
+
+            if (Notice.TeamId != null)
+            {
+                string teamName = Context.Teams
+                    .Find(Notice.TeamId)
+                    .TeamName;
+
+                return teamName;
+            }
+
+            if (Notice.ProjectId != null)
+            {
+                string projectName = Context.Projects
+                    .Find(Notice.ProjectId)
+                    .ProjectName;
+
+                return projectName;
+            }
+
+            return "";
         }
     }
 }
---
+++ wwwroot/js/publicitystatus-listener.js
@@ -0,0 +1,23 @@
+﻿// Hide groups on page load
+let teamGroup = document.getElementById("team-group");
+let projectGroup = document.getElementById("project-group");
+
+teamGroup.style.display = "none";
+projectGroup.style.display = "none";
+
+// Add an event listener to the PublicityStatus dropdown
+document.getElementById("publicityStatusDropdown").addEventListener("change", function () {
+    var selectedValue = this.value;
+
+    // Depending on the selected value, show/hide the form groups
+    if (selectedValue === "Company") {
+        teamGroup.style.display = "none"; // Hide TeamId
+        projectGroup.style.display = "none"; // Hide ProjectId
+    } else if (selectedValue === "Project") {
+        projectGroup.style.display = "block"; // Show TeamId
+        teamGroup.style.display = "none"; // Hide ProjectId
+    } else if (selectedValue === "Team") {
+        projectGroup.style.display = "none"; // Hide TeamId
+        teamGroup.style.display = "block"; // Show ProjectId
+    }
+});
