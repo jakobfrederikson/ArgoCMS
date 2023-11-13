bee7c0909805422f4a123fdf18ed723f209b27b0
Jakob <jakobfred1999@gmail.com>
2023-11-13 13:47:56 +1300

    Redirect to dashboard if user goes to index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ArgoCMS.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
-
+            Redirect("/Dashboard");
         }
     }
 }
