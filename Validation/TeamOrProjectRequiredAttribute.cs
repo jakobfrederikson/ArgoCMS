26b13154ec8b214aadf61aa0af28225ebc08db05
Jakob <jakobfred1999@gmail.com>
2023-11-14 16:21:13 +1300

    Add validation attribute for new nullable team and project id in Job

---
+++ Validation/TeamOrProjectRequiredAttribute.cs
@@ -0,0 +1,20 @@
+﻿using ArgoCMS.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArgoCMS.Validation
+{
+    public class TeamOrProjectRequiredAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = (Job)validationContext.ObjectInstance;
+
+            if (model.TeamID == null && model.ProjectID == null)
+            {
+                return new ValidationResult("You must select either a team or a project (or both).");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
