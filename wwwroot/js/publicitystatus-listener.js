7ab34842b8de8bb4debeb8b1e2815f747763f626
Jakob <jakobfred1999@gmail.com>
2023-11-16 11:45:17 +1300

    Handle case if page receives a team or project ID.

--- wwwroot/js/publicitystatus-listener.js
+++ wwwroot/js/publicitystatus-listener.js
@@ -1,23 +1,36 @@
-﻿// Hide groups on page load
-let teamGroup = document.getElementById("team-group");
+﻿let teamGroup = document.getElementById("team-group");
 let projectGroup = document.getElementById("project-group");
 
 teamGroup.style.display = "none";
 projectGroup.style.display = "none";
 
-// Add an event listener to the PublicityStatus dropdown
-document.getElementById("publicityStatusDropdown").addEventListener("change", function () {
-    var selectedValue = this.value;
+$(document).ready(function () {
+    // Hide groups on page load
+    const publicityStatusDropdown = document.getElementById("publicityStatusDropdown");
 
-    // Depending on the selected value, show/hide the form groups
-    if (selectedValue === "Company") {
-        teamGroup.style.display = "none"; // Hide TeamId
-        projectGroup.style.display = "none"; // Hide ProjectId
-    } else if (selectedValue === "Project") {
-        projectGroup.style.display = "block"; // Show TeamId
-        teamGroup.style.display = "none"; // Hide ProjectId
-    } else if (selectedValue === "Team") {
-        projectGroup.style.display = "none"; // Hide TeamId
-        teamGroup.style.display = "block"; // Show ProjectId
+    if (publicityStatusDropdown.value != null) {
+        console.log("Publicity status: ", publicityStatusDropdown.value);
+        ChangeSelectListDisplay(publicityStatusDropdown.value);
+    }    
+
+    // Add an event listener to the PublicityStatus dropdown
+    publicityStatusDropdown.addEventListener("change", function () {
+        var selectedValue = publicityStatusDropdown.value;
+        console.log("wtf!");
+        ChangeSelectListDisplay(selectedValue);
+    });
+
+    function ChangeSelectListDisplay(selectedValue) {
+        // Depending on the selected value, show/hide the form groups
+        if (selectedValue === "Company") {
+            teamGroup.style.display = "none"; // Hide TeamId
+            projectGroup.style.display = "none"; // Hide ProjectId
+        } else if (selectedValue === "Project") {
+            projectGroup.style.display = "block"; // Show TeamId
+            teamGroup.style.display = "none"; // Hide ProjectId
+        } else if (selectedValue === "Team") {
+            projectGroup.style.display = "none"; // Hide TeamId
+            teamGroup.style.display = "block"; // Show ProjectId
+        }
     }
 });
