392bd56f71a32bb888398bae6e3b4d204c9377ce
Jakob <jakobfred1999@gmail.com>
2023-11-16 14:06:56 +1300

    Comment out EnsureDeleted

--- Program.cs
+++ Program.cs
@@ -87,7 +87,7 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ApplicationDbContext>();
-    context.Database.EnsureDeleted();
+    //context.Database.EnsureDeleted();
     //context.Database.EnsureCreated();
     //context.Database.Migrate();
     // requires using Microsoft.Extensions.Configuration;
