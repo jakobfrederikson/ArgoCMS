c5178c1d930a922de10dbd63b92db6d46532787d
Jakob <jakobfred1999@gmail.com>
2023-11-17 13:51:20 +1300

    Add create page for teams and projects

--- Pages/Projects/Create.cshtml.cs
+++ Pages/Projects/Create.cshtml.cs
@@ -7,16 +7,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ArgoCMS.Data;
 using ArgoCMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using ArgoCMS.Models.JointEntities;
 
 namespace ArgoCMS.Pages.Projects
 {
-    public class CreateModel : PageModel
+    [Authorize(Roles = "Administrators")]
+    public class CreateModel : DependencyInjection_BasePageModel
     {
-        private readonly ArgoCMS.Data.ApplicationDbContext _context;
-
-        public CreateModel(ArgoCMS.Data.ApplicationDbContext context)
+        public CreateModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<Employee> userManager)
+            : base(context, authorizationService, userManager)
         {
-            _context = context;
         }
 
         public IActionResult OnGet()
@@ -31,13 +36,34 @@ namespace ArgoCMS.Pages.Projects
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+            if (Project.ProjectName == null ||
+                Project.ProjectDescription == null ||
+                Project.DueDate == DateTime.MinValue)
             {
                 return Page();
             }
 
-            _context.Projects.Add(Project);
-            await _context.SaveChangesAsync();
+            var currentUser = await UserManager.GetUserAsync(User);
+            if(currentUser == null)
+            {
+                return Page();
+            }
+
+            Project.OwnerID = currentUser.Id;
+            Project.DateCreated = DateTime.UtcNow;
+            Project.ProjectStatus = ProjectStatus.NotStarted;
+
+            Context.Projects.Add(Project);
+            await Context.SaveChangesAsync();
+
+            // Create EmployeeProject entry.
+            Context.EmployeesProjects.Add(new EmployeeProject
+            {
+                EmployeeId = currentUser.Id,
+                ProjectId = Project.ProjectId
+            });
+
+            Context.SaveChanges();
 
             return RedirectToPage("./Index");
         }
--- Pages/Teams/Create.cshtml.cs
+++ Pages/Teams/Create.cshtml.cs
@@ -1,12 +1,61 @@
+using ArgoCMS.Data;
+using ArgoCMS.Models;
+using ArgoCMS.Models.JointEntities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ArgoCMS.Pages.Teams
 {
-    public class CreateModel : PageModel
+    [Authorize(Roles = "Administrators")]
+    public class CreateModel : DependencyInjection_BasePageModel
     {
-        public void OnGet()
+        public CreateModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<Employee> userManager)
+            : base(context, authorizationService, userManager)
         {
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Team Team { get; set; }
+
+        public async Task<IActionResult> OnPostAsync() 
+        {
+            if (Team.TeamName == null ||
+                Team.TeamDescription == null)
+            {
+                return Page();
+            }
+
+            var currentUser = await UserManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                return Page();
+            }
+            
+            Team.DateCreated = DateTime.Now;
+            Team.CreatedById = currentUser.Id;
+
+            Context.Teams.Add(Team);
+            await Context.SaveChangesAsync();
+
+            // Create EmployeeTeam entry
+            Context.EmployeeTeams.Add(new EmployeeTeam
+            {
+                EmployeeId = currentUser.Id,
+                TeamId = Team.TeamId,
+            });
+            Context.SaveChanges();
+
+            return RedirectToPage("./Index");
         }
     }
 }
